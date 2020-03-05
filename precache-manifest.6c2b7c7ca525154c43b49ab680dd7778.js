dc87c8d7ef9398dba5cf341dbd4a4b3dba3f51be
Lucas van den Bemd <lucasvandenbemd@gmail.com>
2020-03-05 14:34:02 +0100

    Updates

---
+++ precache-manifest.6c2b7c7ca525154c43b49ab680dd7778.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d2c46abed9ca11a0f8122017b9516c",
+    "url": "/clovercoats/index.html"
+  },
+  {
+    "revision": "8abc9bc8bdcd779e8cff",
+    "url": "/clovercoats/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "bf246aa9963b0abf09d4",
+    "url": "/clovercoats/static/css/main.888bcb0c.chunk.css"
+  },
+  {
+    "revision": "8abc9bc8bdcd779e8cff",
+    "url": "/clovercoats/static/js/2.91f9d000.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/clovercoats/static/js/2.91f9d000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf246aa9963b0abf09d4",
+    "url": "/clovercoats/static/js/main.3f7a8757.chunk.js"
+  },
+  {
+    "revision": "11c7f162c5209a4b7d09",
+    "url": "/clovercoats/static/js/runtime-main.fbea59fb.js"
+  }
+]);
