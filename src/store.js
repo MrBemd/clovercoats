954d7bce23d88747c60e97b0cc5be8b6580dc297
Lucas van den Bemd <lucasvandenbemd@gmail.com>
2020-03-05 16:59:21 +0100

    Persist state in localStorage

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit';
 import scoreReducer from './features/scoreList/scoreListSlice';
+import {save, load} from 'redux-localstorage-simple';
 
 export default configureStore({
   reducer: {
     score: scoreReducer
-  }
+  },
+  preloadedState: load(),
+  middleware: [save(), ...getDefaultMiddleware()]
 });
